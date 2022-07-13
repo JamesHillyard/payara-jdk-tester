1598e9ed77afb241363194bf71e4a251a8e225eb
JamesHillyard <james.hillyard@payara.fish>
2022-07-13 10:37:15 +0100

    Payara6 Branch

--- src/main/java/pt/test/cmg/rest/RestApplication.java
+++ src/main/java/pt/test/cmg/rest/RestApplication.java
@@ -2,9 +2,9 @@ package pt.test.cmg.rest;
 
 import java.util.HashSet;
 import java.util.Set;
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/")
 @ApplicationScoped
